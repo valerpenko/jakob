c97b586d7efadb99ec75f54f9f642773b6b31d0d
USER <yakov.kosenkov@gmail.com>
2020-10-05 16:42:41 +0300

    door and box

--- simple.py
+++ simple.py
@@ -1,3 +1,9 @@
-p=2
-print(p+p)
-print(p*3)
+startYear,stopYear=map(int,input().split())
+months=[31,28,31,30,31,30,31,31,30,31,30,31]  #длинна месецов
+res=[13]
+for y in range(startYear,stopYear):
+        for m in range(0,12):
+            res.append(res[len(res)-1]+months[m])
+            if m==1 and y%4==0:
+                res[len(res) - 1]+=1
+print(res)
