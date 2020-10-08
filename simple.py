d4b6c4bb207615e16f87ca5915d94fd20fd883ba
USER <yakov.kosenkov@gmail.com>
2020-10-08 18:33:08 +0300

    ф

--- simple.py
+++ simple.py
@@ -1,8 +1,8 @@
 startYear,stopYear=map(int,input().split())
-months=[31,28,31,30,31,30,31,31,30,31,30,31]  #длинна месецов
+months=[31,28,31,30,31,30,31,31,30,31,30]  #длинна месецов
 res=[13]
-for y in range(startYear,stopYear):
-        for m in range(0,12):
+for y in range(startYear,stopYear+1):
+        for m in range(0,11):
             res.append(res[len(res)-1]+months[m])
             if m==1 and y%4==0:
                 res[len(res) - 1]+=1
